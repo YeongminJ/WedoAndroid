7b6b6c08872c5bd05fe6fae8fc127e0576378b2c
Ym <prjym87@gmail.com>
2023-07-31 17:49:38 +0900

    Insert Column With Flaot Action

--- wedo_compose/src/main/java/com/jdi/wedo/MainActivity.kt
+++ wedo_compose/src/main/java/com/jdi/wedo/MainActivity.kt
@@ -4,13 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
@@ -19,6 +23,10 @@ import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -30,17 +38,24 @@ class MainActivity: ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val mutableItems = remember { mutableStateListOf("AAAA","BBBB","CCCC") }
             Scaffold(
                 bottomBar = {MainScreen()},
                 floatingActionButton = {
-                    FloatingActionButton(onClick = { /* ... */ }) {
-                        Text("Hello")
+                    FloatingActionButton(onClick = {
+                        mutableItems.add("DDDD")
+                    }) {
+
                     }
                 }
 
             ) { innerPadding->
-                Box(modifier = Modifier.padding(innerPadding)) {
+
+                LazyColumn(modifier = Modifier.padding(innerPadding)) {
                     /* ... */
+                    items(mutableItems.size) {
+                        TodoItem(mutableItems[it])
+                    }
                 }
             }
 //            Scaffold(
@@ -79,14 +94,23 @@ class MainActivity: ComponentActivity() {
     }
 }
 
-val items = listOf("Wedo", "BucketList", "Group")
+@Composable
+fun TodoItem(itemStr: String) {
+    Card(Modifier.padding(12.dp)
+        .border(width = 4.dp, color = Color.Black)) {
+        Box(contentAlignment = Alignment.Center) {
+            Text(itemStr)
+        }
+    }
+}
 
+val navItems = listOf("Wedo", "BucketList", "Group")
 @Preview(showBackground = true)
 @Composable
 fun MainScreen() {
     var selectedIndex = 0
     NavigationBar {
-        items.forEachIndexed { index, item ->
+        navItems.forEachIndexed { index, item ->
             NavigationBarItem(
                 icon = { Icon(Icons.Filled.Favorite, contentDescription = item) },
                 label = { Text(item) },
